dc81f74a7d8f075495918c2788cf17a8dc97840c
AhmedHAbouIsmail <47505765+AhmedHAbouIsmail@users.noreply.github.com>
2019-03-02 21:25:44 +0200

    Update Vacancy.js

--- models/Vacancy.js
+++ models/Vacancy.js
@@ -1,1 +1,12 @@
-//---
+var id = 1
+var i = 1
+class Vacancy{
+    constructor(description,duration,members)
+    {
+        this.description = description
+        this.duration = duration
+        this.members = members
+        this.id = ""+i++;
+    };
+};
+module.exports = Vacancy
