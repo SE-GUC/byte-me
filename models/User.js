6c2fe5b291f44fb0d6ef54530ef7a77418e119fe
ZizoAbudeif <47536904+ZizoAbudeif@users.noreply.github.com>
2019-03-02 21:46:45 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,14 @@
-//---
+const uuid = require('uuid')
+var i=1
+// The User Model
+class User {
+    
+    constructor(name, email, password) {
+        this.name = name;
+        this.email = email
+        this.password = password
+        this.id = uuid.v4();
+    };
+};
+
+module.exports = User
