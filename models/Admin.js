aed77ec901aad5d461f2c1ef2ef9098e976d33a2
AhmedHAbouIsmail <47505765+AhmedHAbouIsmail@users.noreply.github.com>
2019-03-02 21:16:11 +0200

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,1 +1,11 @@
-//---
+const User = require('../api/User')
+class Admin extends User{
+    constructor(name,age,dateJoined,email,password)
+    {   super(name,email,password);
+        this.dateJoined = dateJoined
+        this.age=age;
+    }
+    
+}
+
+module.exports = Admin
