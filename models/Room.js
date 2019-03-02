108932a3b9f7e77e4feb550e76ef318ca3cd2ac6
AhmedHAbouIsmail <47505765+AhmedHAbouIsmail@users.noreply.github.com>
2019-03-02 21:24:35 +0200

    Update Room.js

--- models/Room.js
+++ models/Room.js
@@ -1,1 +1,10 @@
-//---
+class Room {
+    constructor(roomNo,schedule,rate,capacity) {
+        this.roomNo=roomNo;
+        this.schedule=schedule;
+        this.rate=rate;
+        this.capacity=capacity;
+    };
+}
+
+module.exports = Room;
