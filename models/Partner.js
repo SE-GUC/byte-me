8b6b6aa5662831a24b6347ff4c27f7610c9294dc
AhmedHAbouIsmail <47505765+AhmedHAbouIsmail@users.noreply.github.com>
2019-03-02 21:23:25 +0200

    Update Partner.js

--- models/Partner.js
+++ models/Partner.js
@@ -1,1 +1,16 @@
-//---
+const User = require('../models/User');
+class Partner extends User{
+    constructor(name,age,email,password,partners,boardmembers,events,field,vacancies,pastprojects,feedback)
+    {   super(name,email,password);
+        this.partners=partners;
+        this.boardmembers=boardmembers;
+        this.events=events;
+        this.field=field;
+        this.vacancies=vacancies;
+        this.pastprojects=pastprojects;
+        this.feedback=feedback;
+        this.age=age
+    }
+
+};
+module.exports = Partner
