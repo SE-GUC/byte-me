5797583fbabcb4823a58a7f773a9847a97896e0c
ZizoAbudeif <aboudyadel007@gmail.com>
2019-03-22 20:49:46 +0200

    222

--- models/Member.js
+++ models/Member.js
@@ -1,16 +1,86 @@
-const User = require('../models/User')
-class Member extends User{
-    constructor(name,age,skills,interests,certificates,events,projects,reviews,email,password)
-    {   super(name,email,password);
-        this.skills=skills;
-        this.interests=interests;
-        this.certificates=certificates;
-        this.events=events;
-        this.projects=projects;
-        this.reviews=reviews;
-        this.age=age;
-    }
-    
-}
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-module.exports = Member
+// Create the schema
+const MemberSchema = new Schema({
+    ID: {
+        type: Number,
+        required: true
+    },
+
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+    expiryDate: {
+        type: Date,
+        required: true
+
+    },
+
+    dateOfBirth: {
+        type: Date,
+        required:true
+    },
+
+    age:{
+        type: Number,
+        required:true
+    },
+
+    skills:{
+        type: [String],
+        required:false
+    },
+
+    interests: {
+        type: [String],
+        required:false
+    },
+
+    password: {
+        type: String,
+        required:true
+    },
+
+    pastEventsAttended:{
+        type: [String],
+        required:false
+    },
+
+    contractTime:{
+        type: String,
+        required:true
+
+    },
+    contractLocation:{
+        type:String,
+        required:true
+    },
+
+    workCompleted:{
+        type:[String],
+        required:true
+    },
+
+    reviews:{
+        type:[String],
+        required:true
+    },
+
+    status:{
+        type: boolean,
+        required:true
+    },
+})
+module.exports = Member = mongoose.model('books', MemberSchema)
