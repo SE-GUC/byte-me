1fac190af0fbb9fb32004beb1a27e29c114bf48a
ZizoAbudeif <aboudyadel007@gmail.com>
2019-03-22 09:54:45 +0200

    update

---
+++ Desktop/Lirten-Hub/models/Member.js
@@ -0,0 +1,16 @@
+const User = require('../api/User')
+class member extends User{
+    constructor(name,age,skills,interests,certificates,events,projects,reviews,email,password)
+    {   super(name,email,password);
+        this.skills=skills;
+        this.interests=interests;
+        this.certificates=certificates;
+        this.events=events;
+        this.projects=projects;
+        this.reviews=reviews;
+        this.age=age;
+    }
+    
+}
+
+module.exports = member//---
