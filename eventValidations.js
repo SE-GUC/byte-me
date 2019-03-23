3efb481d0b14885802560fb5f2366a5d48c8650d
Kelloo <47078658+Kelloo@users.noreply.github.com>
2019-03-23 23:22:00 +0200

    Add files via upload

---
+++ eventValidations.js
@@ -0,0 +1,34 @@
+const Joi = require('joi')
+
+module.exports = {
+    createValidation: request => {
+        const createSchema = {
+            type: Joi.string().min(3).max(500).required(),
+            eventName: Joi.string().min(3).max(500).required(),
+            eventDescription: Joi.string().min(3).max(500).required(),
+            eventLocation: Joi.string().min(3).max(500).required(),
+            eventDuration: Joi.string().min(3).max(500).required(),
+            eventDate: Joi.date.required(),
+            attendees:Joi.array.string().min(3).max(500).required(),
+            organizedBy: Joi.string().min(3).max(500).required()
+        }
+
+        return Joi.validate(request, createSchema)
+    },
+
+    updateValidation: request => {
+        const updateSchema = {
+            type: Joi.string().min(3).max(500),
+            eventName: Joi.string().min(3).max(500),
+            eventDescription: Joi.string().min(3).max(500),
+            eventLocation: Joi.string().min(3).max(500),
+            eventDuration: Joi.string().min(3).max(500),
+            eventDate: Joi.date,
+            attendees:Joi.array.string().min(3).max(500),
+            organizedBy: Joi.string().min(3).max(500)
+        
+        }
+
+        return Joi.validate(request, updateSchema)
+    }, 
+}
