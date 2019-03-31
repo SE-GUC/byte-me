f6e1a83ceb7df0da2b642cb74234872d3bd2ba56
YKZidan <youssefzidan2010@gmail.com>
2019-03-31 20:30:36 +0200

    z

--- tests/coworkingfn.js
+++ tests/coworkingfn.js
@@ -2,22 +2,22 @@ const axios = require('axios');
 axios.defaults.adapter = require('axios/lib/adapters/http')
 const functions = {
     getcoworking: async () =>{  
-        const coworkinggetter = await axios.get('http://localhost:8000/api/coworking')
+        const coworkinggetter = await axios.get('http://localhost:3000/api/coworking')
         return coworkinggetter.data
     }, 
     updatecoworking: async (id,desc) =>{
-        const coworkingupdate = await axios.put('http://localhost:8000/api/coworking/update/'+id,{coworkingDescription:desc})
+        const coworkingupdate = await axios.put('http://localhost:3000/api/coworking/update/'+id,{coworkingDescription:desc})
         console.log(coworkingupdate)
         return coworkingupdate
     },
     deletecoworking: async (id) =>{
-        const coworkingdelete = await axios.delete('http://localhost:8000/api/coworking/delete/'+id,{})
+        const coworkingdelete = await axios.delete('http://localhost:3000/api/coworking/delete/'+id,{})
         return coworkingdelete
     }   ,
     postcoworking : async () =>{
         return axios({
             method:'post',
-            url:'localhost:8000/api/coworking/create',
+            url:'localhost:3000/api/coworking/create',
             headers:{'Content-Type':'application/json'},
             data:{
                 type:"Testing Post",
@@ -32,7 +32,7 @@ const functions = {
     },
     
     getcoworkingdesc: async(id)=>{
-        const gete =await axios.get('http://localhost:8000/api/coworking/'+id,{})
+        const gete =await axios.get('http://localhost:3000/api/coworking/'+id,{})
         console.log(gete.data.data)
         return gete.data
 
