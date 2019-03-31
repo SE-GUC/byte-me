b922e7aa160d5c4c1abf34dbf9768f4ab87a0caa
heshaam7 <hesham.kamal@student.guc.edu.eg>
2019-03-31 16:03:39 +0300

    update vacancy 2

--- validations/memberValidations.js
+++ validations/memberValidations.js
@@ -3,22 +3,22 @@ const Joi = require('joi')
 module.exports = {
     createValidation: request => {
         const createSchema = {
-            ID: Joi.number().min(3).max(20).required(),
-            firtsname: Joi.string().min(3).max(20).required(),
-            lastname: Joi.string().min(3).max(20).required(),
-            email: Joi.number().min(7).max(30).required(),
-            expiryDate: Joi.date().min(6).max(30).required(),
-            dateOfBirth: Joi.date().min(1).max(3).required(),
-            age: Joi.number().min(1).max(3).required(),
+           
+            firstName: Joi.string().min(3).max(20).required(),
+            lastName: Joi.string().min(3).max(20).required(),
+            email: Joi.string().min(7).max(30).required(),
+            expiryDate: Joi.date().min(6).max(30),
+            dateOfBirth: Joi.string().min(1).max(3),
+            age: Joi.number().min(1).max(3),
             skills: Joi.array().min(4).max(30),
             interests: Joi.array().min(4).max(30),
-            password: Joi.number().min(8).max(30).required(),
+            password: Joi.number().min(8).max(30),
             pastEventsAttended: Joi.array().min(4).max(30),
-            contractTime: Joi.date().min(6).max(30).required(),
-            contractLocation: Joi.string().min(6).max(30).required(),
-            workComp: Joi.array().min(4).max(30).required(),
-            reviews: Joi.array().min(4).max(30).required(),
-            status: Joi.string().min(4).max(30).required()
+            contractTime: Joi.date().min(6).max(30),
+            contractLocation: Joi.string().min(6).max(30),
+            workComp: Joi.array().min(4).max(30),
+            reviews: Joi.array().min(4).max(30),
+            status: Joi.string().min(4).max(30)
         }
 
         return Joi.validate(request, createSchema)
