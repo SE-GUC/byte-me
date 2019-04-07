b82df9f8befe9eb15ff799e67d34db6bcb98def1
salmasharaby <salmasharaby@hotmail.com>
2019-04-07 20:35:47 +0200

    vacancy react

--- validations/vacancyValidations.js
+++ validations/vacancyValidations.js
@@ -3,14 +3,15 @@ const Joi = require('joi')
 module.exports = {
     createValidation: request => {
         const createSchema = {
-            ownedBy: Joi.string().min(3).max(100).required(),
+            
             description: Joi.string().min(1).max(3000).required(),
             duration: Joi.string().min(1).max(200).required(),
             location: Joi.string().min(1).max(200).required(),
-            monthlyWage: Joi.string().min(1).max(200).required(),
-            startDate: Joi.date(),
-            dailyHours: Joi.number(),
-            endDate: Joi.date()
+            monthlyWage: Joi.number().min(1).max(2000000000000).required(),
+            startDate: Joi.date().required(),
+            dailyHours: Joi.number().required(),
+            endDate: Joi.date().required(),
+            requiredSkills:Joi.string().min(1).max(200)
         }
 
         return Joi.validate(request, createSchema)
@@ -18,14 +19,15 @@ module.exports = {
 
     updateValidation: request => {
         const updateSchema = {
-            ownedBy: Joi.string().min(3).max(100),
+            
             description: Joi.string().min(1).max(3000),
             duration: Joi.string().min(1).max(200),
             location: Joi.string().min(1).max(200),
-            monthlyWage: Joi.string().min(1).max(200),
+            monthlyWage: Joi.number().min(1).max(2000000000000),
             startDate: Joi.date(),
             dailyHours: Joi.number(),
-            endDate: Joi.date()
+            endDate: Joi.date(),
+            requiredSkills:Joi.string().min(1).max(200)
         }
 
         return Joi.validate(request, updateSchema)
