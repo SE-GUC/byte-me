3b3210312ba20f12120cbf67dd4f1d2346963886
AhmedHAbouIsmail <ahmed.abouismail@gmail.com>
2019-04-07 23:28:38 +0200

    one

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import EventPost from'./components/EventPost';
 import VacancyPost from './components/VacancyPost'
+import CoProfile from './components/Coworking/CoProfile';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <EventPost/>
         <VacancyPost/>
+        <CoProfile/>
       </div>
     );
   }
