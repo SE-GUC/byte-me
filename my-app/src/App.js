87dee095227e9270d0fdebb2bc95105d38a17fdb
AhmedHAbouIsmail <ahmed.abouismail@gmail.com>
2019-04-07 23:21:19 +0200

    one

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import CoProfile from './components/Coworking/CoProfile';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <CoProfile/>
+      </div>
+    );
+  }
+}
+
+export default App;
