fddb9d62a70977978b69aab566c1e1463cc40ef2
omneya-hamroush <fleur_blanc186@yahoo.com>
2019-04-14 22:40:35 +0200

    latest partner front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import UpdatePartner from './components/UpdatePartner';
 import GetPartners from './components/GetPartners';
 import GetPartner from './components/GetPartner';
+import DeletePartner from './components/DeletePartner';
+import SearchPartner from './components/SearchPartner';
 import CreatePartner from './components/CreatePartner';
 import './App.css';
 
@@ -10,24 +14,56 @@ class App extends Component {
   render() {
     
     return (
-      <div >
-              <React.Fragment>
-              <UpdatePartner/>
+      <Router>
+      <div className="App">
+        <div className="container">
+          
+          <Header />
+          
+          <Route path="/SignUp" render={props => (
+            <React.Fragment>
+              
+              <CreatePartner  />
+              
+             
+              
+            </React.Fragment>
+
+            
+
+          )} />
+          <Route path="/Partner" render={props => (
+            
+            <React.Fragment>
+              
+            <DeletePartner  />
+             {/* <PartnerViewVacancies />
+             <PartnerViewApplicants /> */}
+
+<UpdatePartner/>
               <br></br>
               <br></br>
               <br></br>
-              <CreatePartner/>
-              <br></br>
-              <br></br>
-              <br></br>
+              
               <GetPartners/>
               <br></br>
               <br></br>
               <br></br>
               <GetPartner/>
-              </React.Fragment>
+             <SearchPartner />
+             
+             
+            
+          </React.Fragment>
+            
+            
+
+          )} />
+        </div>  
       </div>
-      
+    </Router>
+
+     
     );
   }
 }
--- client/src/components/CreatePartner.js
+++ client/src/components/CreatePartner.js
@@ -1,120 +1,210 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from "react-dom";
-import axios from "axios";
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+ class CreatePartner extends Component {
+//    state = {
+//      redirect: false
+//    }
+//  setRedirect = () => {
+//        this.setState({
+//        redirect: true
+//      })
+//    }
+//    renderRedirect = () => {
+//      if (this.state.redirect) {
+//        return <Redirect to='/Partner' />
+//      }
+//    }
 
-class CreatePartner extends Component {
-    constructor (props) {
-        super(props)
-        this.onpartners=this.onpartners.bind(this);
-        this.onboardMembers=this.onboardMembers.bind(this);
-        this.onorganizationName=this.onorganizationName.bind(this)
-        this.onemail = this.onemail.bind(this);
-        this.onpassword= this.onpassword.bind(this);
-        this.ondescription = this.ondescription.bind(this);
-        this.onfieldOfWork = this.onfieldOfWork.bind(this);
-
+    constructor (props){
+        super(props);
+        this.onChangeOrganizationName=this.onChangeOrganizationName.bind(this)
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword= this.onChangePassword.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangePartners = this.onChangePartners.bind(this);
+        this.onChangeBoardMembers=this.onChangeBoardMembers.bind(this);
+        /* this.onChangePartner1 = this.onChangePartner1.bind(this);
+        this.onChangePartner2 = this.onChangePartner2.bind(this);
+        this.onChangePartner3 = this.onChangePartner3.bind(this);
+        this.onChangeBoardMember1=this.onChangeBoardMember1.bind(this);
+        this.onChangeBoardMember2=this.onChangeBoardMember2.bind(this);
+        this.onChangeBoardMember3=this.onChangeBoardMember3.bind(this);  */
+        this.onChangeFieldOfWork=this.onChangeFieldOfWork.bind(this);
+        
         this.onSubmit = this.onSubmit.bind(this);
-
         this.state = {
-            partners:'',
-            boardMembers:'',
-            organizationName:'',
-            email:'',
+            organizationName: '',
+            email: '',
             password: '',
             description: '',
-            fieldOfWork:'' 
+            partners: '',
+            boardMembers: '',
+           /*  partner1:'',
+            partner2:'',
+            partner3:'',
+            boardMembers:[],
+            boardMember1: '',
+            boardMember2: '',
+            boardMember3: '', */
+
+            fieldOfWork: ''
+           
+           
         }
+        
+    
+       
     }
-    onpartners(e) {
-        this.setState({
-          partners: e.target.value
-        });
-      }
-    onboardMembers(e) {
-        this.setState({
-          boardMembers: e.target.value
-        });
-      }
-    onorganizationName(e) {
+
+    onChangeOrganizationName(e) {
         this.setState({
           organizationName: e.target.value
-        })
+        });
       }
-    onemail(e) {
+    onChangeEmail(e) {
         this.setState({
           email: e.target.value
-        })
+        });
       }
-    onpassword(e) {
+    onChangePassword(e) {
         this.setState({
           password: e.target.value
         })
       }
-    ondescription(e) {
+    onChangeDescription(e) {
         this.setState({
           description: e.target.value
         })
       }
-      onfieldOfWork(e) {
+     /* onChangePartner1(e) {
+         this.setState({
+           partner1: e.target.value
+         })
+       }
+       onChangePartner2(e) {
+         this.setState({
+           partner2: e.target.value
+         })
+       }
+       onChangePartner3(e) {
+         this.setState({
+           partner3: e.target.value
+         })
+       } */
+       onChangePartners(e) {
+        this.setState({
+          partners: e.target.value
+        })
+      }
+     onChangeBoardMembers(e) {
+         this.setState({
+           boardMembers: e.target.value
+         })
+       }
+      /*  onChangeBoardMember2(e) {
+         this.setState({
+           boardMember2: e.target.value
+         })
+      }
+       onChangeBoardMember3(e) {
+         this.setState({
+           boardMember3: e.target.value
+         })
+     } */
+      onChangeFieldOfWork(e) {
         this.setState({
           fieldOfWork: e.target.value
         })
       }
-    
-      onSubmit(e) {
-        e.preventDefault();
-        axios.post('http://localhost:4000/api/partner/', {
-            partners:this.state.partners,
-            boardMembers: this.state.boardMembers,
-            organizationName: this.state.organizationName,
-            email:this.state.email,
-            password: this.state.password,
-            description: this.state.description,
-            fieldOfWork:this.state.fieldOfWork
-        }, {
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-        .then(res => {
-            console.log(res.data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    
+      
+  onSubmit = (e) => {
+    e.preventDefault();
+    /*const obj ={
+        organizationName:this.state.organizationName,
+        email:this.state.email,
+        password:this.state.password,
+        description:this.state.description,
+        // partners:this.state.partners.concat([this.state.partner1,this.state.partner2,this.state.partner3]),
+        // partner1:this.state.partner1,
+        // partner2:this.state.partner2,
+        // partner3:this.state.partner3,
+        // boardMembers:this.state.boardMembers.concat([this.state.boardMember1,this.state.boardMember2,this.state.boardMember3]),
+        // boardMember1:this.state.boardMember1,
+        // boardMember2:this.state.boardMember2,
+        // boardMember3:this.state.boardMember3,
+        fieldOfWork:this.state.fieldOfWork
+
+    };
+   */
+  console.log("--------------- "+this.state)
+    axios.post('http://localhost:4000/api/partner/',{
+        organizationName:this.state.organizationName,
+        email:this.state.email,
+        password:this.state.password,
+        description:this.state.description,
+        partners:this.state.partners,
+        boardMembers:this.state.boardMembers,
+        // partners:this.state.partners.concat([this.state.partner1,this.state.partner2,this.state.partner3]),
+        // partner1:this.state.partner1,
+        // partner2:this.state.partner2,
+        // partner3:this.state.partner3,
+        // boardMembers:this.state.boardMembers.concat([this.state.boardMember1,this.state.boardMember2,this.boardMember3]),
+        // boardMember1:this.state.boardMember1,
+        // boardMember2:this.state.boardMember2,
+        // boardMember3:this.state.boardMember3,
+        fieldOfWork:this.state.fieldOfWork
+    })
+    .then(res => {
+        console.log(res.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+    /*this.setState ( {
+        organizationName: '',
+        email: '',
+        password: '',
+        description: '',
+        // partners: [],
+        // partner1:'',
+        // partner2:'',
+        // partner3:'',
+        // boardMembers:[],
+        // boardMember1: '',
+        // boardMember2: '',
+        // boardMember3: '',
+
+        fieldOfWork: ''
+       
+       
+    })*/
   }
   clickMe()
-{
-    // alert('test')
-}
-    render() {
-        return(
-            <div style={{ marginTop: 10 }}>
-            <h3>Create a Partner</h3>
+  {
+      // alert('test')
+     
+      //this.setRedirect();
+     // window.parent.location = window.parent.location.href;
+  }
+  //onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  
+ 
+  render() {
+    return (
+        
+<div style={{ marginTop: 10 }}>
+            
             <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                    <label>Partners: </label>
-                    <input type="text" 
+                    <label>Organization name:  </label>
+                    <input 
+                      type="text" 
                       className="form-control" 
-                      value={this.state.partners}
-                      onChange={this.onpartners}
-                      />
-                </div>
-                <div className="form-group">
-                    <label>Board Members: </label>
-                    <input type="text" 
-                      className="form-control"
-                      value={this.state.boardMembers}
-                      onChange={this.onboardMembers}
-                      />
-                </div>
-                <div className="form-group">
-                    <label>Organization Name: </label>
-                    <input type="text" 
-                      className="form-control"
                       value={this.state.organizationName}
-                      onChange={this.onorganizationName}
+                      onChange={this.onChangeOrganizationName}
                       />
                 </div>
                 <div className="form-group">
@@ -122,7 +212,7 @@ class CreatePartner extends Component {
                     <input type="text" 
                       className="form-control"
                       value={this.state.email}
-                      onChange={this.onemail}
+                      onChange={this.onChangeEmail}
                       />
                 </div>
                 <div className="form-group">
@@ -130,7 +220,7 @@ class CreatePartner extends Component {
                     <input type="text" 
                       className="form-control"
                       value={this.state.password}
-                      onChange={this.onpassword}
+                      onChange={this.onChangePassword}
                       />
                 </div>
                 <div className="form-group">
@@ -138,30 +228,87 @@ class CreatePartner extends Component {
                     <input type="text" 
                       className="form-control"
                       value={this.state.description}
-                      onChange={this.ondescription}
+                      onChange={this.onChangeDescription}
                       />
                 </div>
                 <div className="form-group">
-                    <label>Field of Work: </label>
+                    <label>Partners: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.partners}
+                      onChange={this.onChangePartners}
+                      />
+                </div>
+                {/*  <div className="form-group">
+                    <label>Partner1: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.partner1}
+                      onChange={this.onChangePartner1}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Partner2: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.partner2}
+                      onChange={this.onChangePartner2}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Partner3: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.partner3}
+                      onChange={this.onChangePartner3}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Board member 1: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.boardMember1}
+                      onChange={this.onChangeBoardMember1}
+                      />
+                </div>
+                <div className="form-group">
+                    <label>Board member 2: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.boardMember2}
+                      onChange={this.onChangeBoardMember2}
+                      />
+                </div> */}
+                <div className="form-group">
+                    <label>Board members: </label>
+                    <input type="text" 
+                      className="form-control"
+                      value={this.state.boardMembers}
+                      onChange={this.onChangeBoardMembers}
+                      />
+                </div> 
+                <div className="form-group">
+                    <label>Field of work: </label>
                     <input type="text" 
                       className="form-control"
                       value={this.state.fieldOfWork}
-                      onChange={this.onfieldOfWork}
+                      onChange={this.onChangeFieldOfWork}
                       />
                 </div>
                 <div className="form-group">
-                    <input type="submit" value="Create" className="btn btn-primary" onClick ={this.clickMe.bind(this)} />
+                 {/* {this.renderRedirect()}  */}
+                    <input type="submit" value="Sign up" className="btn btn-primary" onClick ={this.clickMe.bind(this)}  />
+                    
+                     
+       
+       
+      
                 </div>
             </form>
         </div>
-        )
-    }
-
+    )
+  }
 }
 
-ReactDOM.render(
-    <CreatePartner subreddit='reactjs' />,
-      document.getElementById('root')
-  )
 
-export default CreatePartner;
+export default CreatePartner
