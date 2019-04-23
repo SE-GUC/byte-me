cf060cff06dd0e3818b7da31feb9430d0340fe93
AhmedHAbouIsmail <47505765+AhmedHAbouIsmail@users.noreply.github.com>
2019-04-23 08:07:04 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomeEvent from "./components/HomeEventPartner";
 
 
 //import Dropdown from 'react-drop-down';
-
-
 import CoHome from './components/Coworking/CoHome';
 import HomeMember from './components/MemberHome';
-
-
 import UpdatePartner from './components/UpdatePartner';
 import GetPartners from './components/GetPartners';
 import GetPartner from './components/GetPartner';
@@ -23,7 +18,6 @@ import SearchOrganizationName from './components/SearchOarganizationName';
 import SignIn from './components/SignIn'
 import SignUpHome from './components/SignUpHome';
 import HomePartner from './components/HomePartner';
-import './App.css';
 import SearchEmail from './components/SearchEmail';
 
 import WhichEntity from "./components/WhichEntity";
